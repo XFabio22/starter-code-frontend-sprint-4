69d74ea83e9579d3a480387eb585bc744fe33d3e
XFabio22 <98523846+XFabio22@users.noreply.github.com>
2022-06-23 12:00:40 +0200

    nivel 1

--- src/films.js
+++ src/films.js
@@ -1,13 +1,18 @@
+
+
+
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
+  const result = movies.map((movie) => movie.director);
   console.log("EXERCICE 1 ->", result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  let nombreDirector = document.getElementById("nombreDirector").value;
+  const result2 = movies.filter(movie => movie.director)
+  console.log("EXERCICE 2 ->",result2)
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
