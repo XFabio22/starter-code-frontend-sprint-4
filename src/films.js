13e3dbe66387e7f70b36faed5718605c08c07998
XFabio22 <98523846+XFabio22@users.noreply.github.com>
2022-06-27 11:52:21 +0200

    Sprint 4

--- src/films.js
+++ src/films.js
@@ -1,4 +1,5 @@
 
+
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
   const result = movies.map((movie) => movie.director);
@@ -44,27 +45,47 @@ function orderAlphabetically(array) {
   })
   
   console.log("EXERCICE 4 ->" ,result4.slice(0,20));
+
 }
 
 // Exercise 5: Order by year, ascending
 function orderByYear() {
-  let result5 = movies.sort((a,b)  =>{
-    if (a.year < b.year){
+  let forYears = movies.sort((a,b) =>  {
+    return a.year - b.year;
+  })
+let froAbc = forYears.sort((a,b)=> {
+  if(a.year === b.year){
+    if (a.title < b.title){
       return -1;
     }
-    if (a.year > b.year){
+    if (a.title > b.title){
       return 1
     }
-    return 0;
-  })
   
-  console.log("EXERCICE 4 ->" ,result5.slice(0,20));
+  }
+})
+  console.log("EXERCICE 5 ->" ,froAbc);
 }
 
 // Exercise 6: Calculate the average of the movies in a category
 function moviesAverageByCategory() {
 
+  let categoria = document.getElementById("categoria").value;
+  let contador = 0;
+  let puntos;
+  const result6 = movies.reduce( (puntosTotal, peli) => {
+    if(peli.genre[length] == categoria &&  peli.score !== ""){
+      contador++;
+      puntosTotal += peli.score;
+    }
+    return puntosTotal; 
+
+  }, 0)
+  puntos = Number ((result6 / contador).toFixed(2));
+  console.log("EXERCICE 6 ->" ,puntos);
+  return puntos;
 }
+
 
 // Exercise 7: Modify the duration of movies to minutes
 function hoursToMinutes() {
